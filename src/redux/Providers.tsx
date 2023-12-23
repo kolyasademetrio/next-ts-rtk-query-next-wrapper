788473be2dcabc92bab494375a9f4657e5d0cf92
dmytro.koliasa@onpoint.to <dmytro.koliasa@onpoint.to>
2023-12-23 14:10:21 +0200

    added makeStore within createReducerManager

--- src/redux/Providers.tsx
+++ src/redux/Providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store } from "./makeStore";
 import { PropsWithChildren } from "react";
 
 const Providers = ({ children }: PropsWithChildren) => {
