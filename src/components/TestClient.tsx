137505d248f8f15a0f473b026dc3665866d26e98
dmytro.koliasa@onpoint.to <dmytro.koliasa@onpoint.to>
2023-12-23 14:53:40 +0200

    added dynamic reducer and DynamicModuleLoader

--- src/components/TestClient.tsx
+++ src/components/TestClient.tsx
@@ -1,12 +1,16 @@
 "use client";
 import { testSelectors, useTestActions } from "../redux/slices/testSlice";
+import { dynamicSelectors, dynamicActions } from "../redux/slices/dynamicSlice";
 import { useSelector } from "react-redux";
+import { DynamicModuleLoader } from "./DynamicModuleLoader";
+import { dynamicReducer } from "../redux/slices/dynamicSlice";
 
 const TestClient = () => {
    // get count with selector function directly
    // const count = useSelector((state: any) => state.test.count);
 
    const count = useSelector(testSelectors.getTestCount);
+   const dynamicCount = useSelector(dynamicSelectors.getDynamicCount);
 
    const { add, subtract, addNumber } = useTestActions();
 
@@ -22,12 +26,17 @@ const TestClient = () => {
       addNumber(5);
    };
 
+   console.log(dynamicCount);
+
    return (
       <>
          <div>
             <button onClick={addHandler}>+</button>
             <button onClick={subtractHandler}>-</button>
-            <button onClick={addNumberHandler}>add 5</button>
+            {dynamicCount}
+            <DynamicModuleLoader reducers={{ dynamic: dynamicReducer }}>
+               <button onClick={addNumberHandler}>add 5</button>
+            </DynamicModuleLoader>
          </div>
          <div>{count}</div>
       </>
